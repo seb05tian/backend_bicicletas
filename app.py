9464167c18bbc0e53e3eea18155125e5794eaab8
sebastian <sebastiancamposvilla2004@gmail.com>
2023-10-09 18:41:22 -0500

    change

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
-from flask import Flask, redirect, jsonify, render_template, request, json, session
+from flask import Flask, redirect, jsonify, render_template, request, session
 from config.bd import app, db
+from sqlalchemy import text
+
 
 from api.usuario import ruta_usuario
-from api.Ruta import  ruta_ruta
-from api.comunidad import  ruta_comunidad
-from api.alerta import  ruta_alerta
+from api.Ruta import ruta_ruta
+from api.comunidad import ruta_comunidad
+from api.alerta import ruta_alerta
 
 app.register_blueprint(ruta_usuario, url_prefix="/api")
 app.register_blueprint(ruta_ruta, url_prefix="/api")
 app.register_blueprint(ruta_comunidad, url_prefix="/api")
 app.register_blueprint(ruta_alerta, url_prefix="/api")
 
-#Templates Front
 
-@app.route('/', methods=['GET'])
-def index():
-    
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'usuario' in request.form and 'password' in request.form:
+        _user = request.form['usuario']
+        _pass = request.form['password']
+
+        sql_query = text('SELECT * FROM tblusuario WHERE nombre_usuario = :user AND contraseña = :pass')
+        result = db.session.execute(sql_query, {'user': _user, 'pass': _pass})
+        account = result.fetchone()
+
+        if account:
+            # Accede a los elementos de la tupla por posición numérica
+            id_usuario = account[0]
+            nombre_usuario = account[1]
+            contraseña = account[2]
+
+            session['logueado'] = True
+            session['id_usuario'] = id_usuario
+            return render_template("index.html")
+
     return render_template("login.html")
+
+
+
 
 @app.route('/index', methods=['GET'])
 def inicio():
